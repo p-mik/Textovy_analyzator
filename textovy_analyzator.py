b3e124f4b3413c9c70be9207b686fc57abfc6496
p-mik <petr.mikulka@gmail.com>
2024-04-22 23:02:18 +0200

    Přidal jsem ověření uživatele

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -6,8 +6,6 @@ discord: p_mik Mik#7555
 """
 from task_template import TEXTS
 
-#print(str(TEXTS[0]))
-
 # mám seznam uživatelů jako víceřádkový string
 registrovani_uzivatele = """
 +------+-------------+
@@ -30,12 +28,34 @@ for prvek in seznam.copy():
     if not prvek.isalnum():
         seznam.remove(prvek)
 
+# odstraním z listu první dva prvky (user a password)
+seznam = seznam[2:]
+
 # naplním z listu slovník tak, že sudá položka je key a lichá value
 seznam_uzivatelu = dict() 
 for x in range(len(seznam)):
     if x % 2 == 0:
         seznam_uzivatelu[seznam[x]] = seznam[x+1]
 
-print(str(seznam_uzivatelu))
+# teď přihlašuji uživatele
+print("-" * 50)
+user = input("Ahoj uživateli! Zadej své uživatelské jméno:\n")
 
-#print(seznam)
+# ověřuji, jestli známe uživatele
+if user in seznam_uzivatelu:
+    password = input(f"Vítej {user}, jaké je tvé heslo? \n")
+else:
+    print(f"Jméno {user} není registrováno. Loučím se")
+    exit()
+
+# ověřuji správné heslo
+print("-" * 50)
+if password == seznam_uzivatelu[user]:
+    print(f"{user}, vítej v aplikaci!")
+    print()
+    print(f"Dnes máme k analýze {len(TEXTS)} texty")
+    cislo_textu = input(f"Zvol číslo textu, který budeme analyzovat: \n")
+
+else:
+    print(f"Heslo nesedí, musíš začít znovu...")
+    exit()
