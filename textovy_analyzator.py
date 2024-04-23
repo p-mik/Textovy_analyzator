80c83545c99d8a71e195c6cbb9c58bb35d75c418
p-mik <petr.mikulka@gmail.com>
2024-04-23 21:37:53 +0200

    Přidána textová analýza, chybí dodělat graf

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -29,3 +29,57 @@ if password == seznam_uzivatelu[user]:
 else:
     print(f"Heslo nesedí, musíš začít znovu...")
     exit()
+
+# Kontrola, zda klient zvolil číslo a text v rozmezí
+
+if not cislo_textu.isdigit() or int(cislo_textu) < 1 or int(cislo_textu) > 3:
+    print(f"Bohužel jsi nezvolil v požadovaném rozmezí. Končíme...")
+    exit()
+else:
+    cislo_textu = int(cislo_textu)
+  
+
+print(TEXTS[cislo_textu - 1])
+# Příprava proměnných
+pocet_slov = 0
+zacina_velkym = 0
+vse_velkym = 0
+vse_malym = 0
+pocet_cisel = 0
+suma_cisel = 0
+
+#ITERACE NAD VYBRANÝM TEXTEM
+for slovo in TEXTS[cislo_textu - 1].split():
+    # POČET SLOV
+    pocet_slov += 1
+
+    # POČET SLOV ZAČÍNAJÍCÍCH VELKÝM PÍSMENEM
+    if slovo.istitle():
+        zacina_velkym += 1
+    
+    # POČET SLOV PSANÝCH VELKÝMI PÍSMENY
+    elif slovo.isupper():
+        vse_velkym += 1
+    
+    # POČET SLOV PSANÝCH VELKÝMI PÍSMENY
+    elif slovo.islower():
+        vse_malym += 1
+    
+    # POČET ČÍSEL
+    elif slovo.isdigit():
+        pocet_cisel += 1
+    # SUMA ČÍSEL
+        suma_cisel += int(slovo)
+print()
+print("-" * 50)
+print(f"Počet slov v textu: {pocet_slov}")
+print(f"Počet slov začínajících velkým písmenem: {zacina_velkym}")
+print(f"Počet slov psaných pouze velkými písmeny: {vse_velkym}")
+print(f"Počet slov psaných pouze malými písmeny: {vse_malym}")
+print(f"Počet čísel v textu: {pocet_cisel}")
+print(f"Součet čísel v textu: {suma_cisel}")
+print("-" * 50)
+
+
+
+# GRAF
